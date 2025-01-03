bf995c7daea2c35e7dd96a2424d3828cca902199
KsyhArya <141986389+KsyhArya@users.noreply.github.com>
2025-01-03 18:47:46 +0700

    settings.py

--- CTF/settings.py
+++ CTF/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gte39m!pnxhrdz4a3@xl5v(n-%t!q(u94+*zm-(r%lyk$$$99#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [gunicorn DjangoCTF.wsgi:application]
 
 
 # Application definition
