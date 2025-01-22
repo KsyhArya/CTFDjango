47c32af8d3e527112c1f4031d4aa6227d3ff530b
KsyhArya <141986389+KsyhArya@users.noreply.github.com>
2025-01-22 12:51:48 +0700

    Update settings.py

--- CTF/settings.py
+++ CTF/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'CTFDjango.urls'
+ROOT_URLCONF = 'CTF.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'CTFDjango.wsgi.application'
+WSGI_APPLICATION = 'CTF.wsgi.application'
 
 
 # Database
